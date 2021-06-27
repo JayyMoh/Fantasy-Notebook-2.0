24d986f2ef217acf1a1a443a7ce4789ade5b4257
Jonathan <jonmohr@outlook.com>
2021-06-26 22:15:36 -0400

    Router added and nav bar connects to proper routes

--- fantasy-notebook-frontend/src/App.js
+++ fantasy-notebook-frontend/src/App.js
@@ -9,19 +9,24 @@ import Rookie from './Components/Rookies/Rookie'
 import AddRookie from './Components/Rookies/AddRookie'
 import EditRookie from './Components/Rookies/EditRookie'
 import Home from './Components/Home'
+import { Route, Link } from 'react-router-dom'
+import NavBar from './Components/NavBar'
+import { Styled } from 'styled-components'
 
 function App() {
     return <div className="App">
-      <Home />
-      <Devy />
-      <AddDevy />
-      <EditDevy />
-      <Veteran />
-      <AddVeteran />
-      <EditVeteran />
-      <Rookie />
-      <AddRookie />
-      <EditRookie />
+      <NavBar />
+      <br />
+      <Route exact path='/' component={ Home } />
+      <Route exact path='/rookies' component={ Rookie } />
+      <Route exact path='/add_rookie' component={ AddRookie } />
+      <Route exact path='/edit_rookie' component={ EditRookie } />
+      <Route exact path='/devys' component={ Devy } />
+      <Route exact path='/add_devy' component={ AddDevy } />
+      <Route exact path='/edit_devy' component={ EditDevy } />
+      <Route exact path='/veterans' component={ Veteran } />
+      <Route exact path='/add_veteran' component={ AddVeteran } />
+      <Route exact path='/edit_veteran' component={ EditVeteran } />
     </div>
 }
 
---
+++ fantasy-notebook-frontend/src/Components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: white;
+`
+
+function NavBar() {
+    return(
+        <div className='container'>
+            <nav>
+                <ul className='nav-ul'>
+                    <li className='nav-li'><StyledLink to='/'>Home</StyledLink></li>
+                    <li className='nav-li'><StyledLink to='/devys'>Devys</StyledLink></li>
+                    <li className='nav-li'><StyledLink to='/rookies'>Rookies</StyledLink></li>
+                    <li className='nav-li'><StyledLink to='/veterans'>Veterans</StyledLink></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
